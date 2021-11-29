eab360d2132541581ee023419ee6fc75c7029d95
HBrightShow <angusbright@163.com>
2021-11-29 10:36:09 +0800

    change dir

--- build/release-cpp11/examples/shorturl/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hml/xiaozhong/code/nethings/muduo/examples/shorturl
-# Build directory: /home/hml/xiaozhong/code/nethings/build/release-cpp11/examples/shorturl
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
